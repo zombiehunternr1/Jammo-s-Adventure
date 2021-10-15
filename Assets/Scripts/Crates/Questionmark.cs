bfe96211c10e002771816e8f5d270b30bb0a8a48
zombiehunternr1 <44666850+zombiehunternr1@users.noreply.github.com>
2021-10-15 11:21:41 +0200

    Question crate script added
    - Renamed the regular crate script to Questionmark script because it had almost all the functionalities I wanted my question crate to have

--- Assets/Scripts/Crates/Questionmark.cs
+++ Assets/Scripts/Crates/Questionmark.cs
@@ -2,9 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Regular : MonoBehaviour, ICrateBase
+public class Questionmark : MonoBehaviour, ICrateBase
 {
     public GameObject BrokenCrate;
+    public GameObject Life;
+    public bool IsLife;
     public int RandomDropRange;
     public int DropAmount;
     public List<GameObject> Bolts;
@@ -90,19 +92,26 @@ public class Regular : MonoBehaviour, ICrateBase
 
     private void SpawnBoltTypes()
     {
-        for(int i = 0; i < DropAmount; i++)
+        if (IsLife)
         {
-            if(i > 0)
+            Instantiate(Life, new Vector3(transform.position.x, transform.position.y + YOffset, transform.position.z), Quaternion.identity);
+        }
+        else
+        {
+            for (int i = 0; i < DropAmount; i++)
             {
-                var OffsetX = Random.Range(-RandomDropRange, RandomDropRange);
-                var OffsetZ = Random.Range(-RandomDropRange, RandomDropRange);
-                int SelectedItemDrop = Random.Range(0, Bolts.Count);
-                GameObject ItemDrop = Instantiate(Bolts[SelectedItemDrop], new Vector3(transform.position.x + OffsetX, transform.position.y + YOffset, transform.position.z + OffsetZ), Quaternion.identity);
-            }
-            else
-            {
-                int SelectedItemDrop = Random.Range(0, Bolts.Count);
-                GameObject ItemDrop = Instantiate(Bolts[SelectedItemDrop], new Vector3(transform.position.x, transform.position.y + YOffset, transform.position.z), Quaternion.identity);
+                if (i > 0)
+                {
+                    var OffsetX = Random.Range(-RandomDropRange, RandomDropRange);
+                    var OffsetZ = Random.Range(-RandomDropRange, RandomDropRange);
+                    int SelectedItemDrop = Random.Range(0, Bolts.Count);
+                    Instantiate(Bolts[SelectedItemDrop], new Vector3(transform.position.x + OffsetX, transform.position.y + YOffset, transform.position.z + OffsetZ), Quaternion.identity);
+                }
+                else
+                {
+                    int SelectedItemDrop = Random.Range(0, Bolts.Count);
+                    Instantiate(Bolts[SelectedItemDrop], new Vector3(transform.position.x, transform.position.y + YOffset, transform.position.z), Quaternion.identity);
+                }
             }
         }
         DisableCrate();
@@ -121,6 +130,7 @@ public class Regular : MonoBehaviour, ICrateBase
     {
         if (!IsBroken)
         {
+            IsLife = false;
             IsBroken = true;
             GameManager.Instance.UpdateCrateCount(this);
             gameObject.SetActive(false);
