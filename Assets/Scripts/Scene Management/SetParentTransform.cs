c67b727fcde2e6266764934dd5860422253e88f5
zombiehunternr1 <44666850+zombiehunternr1@users.noreply.github.com>
2021-10-29 16:30:22 +0200

    Broken crate SFX added
    - Added in two different types of crate breaking sound effects so you won't hear the same breaking sound every single time
    - Inside the SetParentTransform script that is attached to the broken regular crate prefab I've added in a public list called CrateBreaking where I store the refences to the different crate breaking sound effects. Once this object gets spawns in it picks a random clip from the list and plays that sound effect.

--- Assets/Scripts/Scene Management/SetParentTransform.cs
+++ Assets/Scripts/Scene Management/SetParentTransform.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class SetParentTransform : MonoBehaviour
 {
+    public List<AudioClip> CrateBreaking;
+
     Transform SpawnItemsHolder;
+    AudioSource AudioSource;
     private void Awake()
     {
+        AudioSource = GetComponent<AudioSource>();
         GameManager.Instance.UpdateItemContainerList(gameObject);
+        int ChosenSFX = Random.Range(0, CrateBreaking.Count);
+        AudioSource.clip = CrateBreaking[ChosenSFX];
+        AudioSource.Play();
     }
-
 }
